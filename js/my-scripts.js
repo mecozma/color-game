91254bd54038972e31a6ffbcb845dcb594f5025c
mecozma <mecozma@gmail.com>
2018-08-03 00:11:03 +0100

    Added a reset button to reset the colors and reset the game at the end

--- js/my-scripts.js
+++ js/my-scripts.js
@@ -13,6 +13,24 @@ let pickedColor = pickColor();
 let colorDisplay = document.querySelector('h1 span');
 let message = document.querySelector('#message');
 let h1 = document.querySelector('h1');
+let resetButton = document.querySelector('#stripe button');
+resetButton.addEventListener('click', () => {
+	// generate new colors
+	colors = generateRandomcolors(6);
+	// pick a new random color that will be the only one right
+	pickedColor = pickColor();
+	// change the colorDisplay to match the picked color;
+	colorDisplay.textContent = pickedColor;
+	// change squares' colors
+	squares.forEach((square, i) => {
+		square.style.backgroundColor = colors[i];
+	});
+	// change the message span text
+	message.textContent = '';
+	// change the h1 background color
+	h1.style.backgroundColor = '#232323';
+	resetButton.textContent = 'New Colors'
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,6 +42,7 @@ squares.forEach((square, i, arr) => {
 	// Add event listeners to each square
 	square.addEventListener('click', () => {
 		if (square.style.backgroundColor === pickedColor) {
+			resetButton.textContent = 'Play Again?';
 			h1.style.backgroundColor = pickedColor;
 			message.textContent = "Correct!";
 			changeColors(pickedColor);
